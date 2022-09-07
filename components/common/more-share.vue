90fb9d25e4b152d711d00acd416995cead4a1160
李绪杰 <1340482172@qq.com>
2022-09-07 22:02:55 +0800

    分享功能

---
+++ components/common/more-share.vue
@@ -0,0 +1,168 @@
+<template>
+	<uni-poput ref="popup" type="bottom">
+		<view class="text-center font-md p-2">分享到</view>
+		<view class="flex align-center p-2 justify-between">
+			<view 
+				class="flex flex-column align-center" 
+				v-for="(item, index) in providerList" :key="index">
+				<view
+					class="rounded-circle text-white flex align-center justify-center" 
+					:class="item.icon"
+					style="width: 130rpx;height: 130rpx;font-size: 60rpx;"
+					:style="item.bgColor"
+					@tap="share(item)">
+				</view>
+				<view class="font-sm text-light-muted">{{item.name}}</view>
+			</view>
+		</view>
+		<view class="text-center border-top font-md p-2">取消</view>
+	</uni-poput>
+</template>
+
+<script>
+	import uniPoput from '@/components/uni-ui/uni-popup/uni-popup.vue'
+	export default {
+		components: {
+			uniPoput
+		},
+		props: {
+			providerList: Array
+		},
+		data() {
+			return {
+				shareType: 1,
+				shareText: 'uni-app可以同时发布成原生App、小程序、H5，邀请你一起体验！'
+			}
+		},
+		// 在组件里面使用， 相当于 vue 生命周期的 created mounted，
+		methods: {
+			open() {
+				this.$refs.popup.open()
+			},
+			close() {
+				this.$refs.popup.close()
+			},
+			// 分享
+			async share(e) {
+				console.log('分享通道:'+ e.id +'； 分享类型:' + this.shareType);
+				if(!this.shareText && (this.shareType === 1 || this.shareType === 0)){
+					uni.showModal({
+						content:'分享内容不能为空',
+						showCancel:false
+					})
+					return;
+				}
+				
+				if(!this.image && (this.shareType === 2 || this.shareType === 0)){
+					uni.showModal({
+						content:'分享图片不能为空',
+						showCancel:false
+					})
+					return;
+				}
+				
+				let shareOPtions = {
+					provider: e.id,
+					scene: e.type && e.type === 'WXSenceTimeline' ? 'WXSenceTimeline' : 'WXSceneSession', 
+					//WXSceneSession”分享到聊天界面，“WXSenceTimeline”分享到朋友圈，“WXSceneFavorite”分享到微信收藏     
+					type: this.shareType,
+					success: (e) => {
+						console.log('success', e);
+						uni.showModal({
+							content: '已分享',
+							showCancel:false
+						})
+					},
+					fail: (e) => {
+						console.log('fail', e)
+						uni.showModal({
+							content: e.errMsg,
+							showCancel:false
+						})
+					},
+					complete:function(){
+						console.log('分享操作结束!')
+					}
+				}
+				
+				switch (this.shareType){
+					case 0:
+						shareOPtions.summary = this.shareText;
+						shareOPtions.imageUrl = this.image;
+						shareOPtions.title = '欢迎体验uniapp';
+						shareOPtions.href = 'https://uniapp.dcloud.io';
+						break;
+					case 1:
+						shareOPtions.summary = this.shareText;
+						break;
+					case 2:
+						shareOPtions.imageUrl = this.image;
+						break;
+					case 5:
+						shareOPtions.imageUrl = this.image ? this.image : 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/app/share-logo@3.png'
+						shareOPtions.title = '欢迎体验uniapp';
+						shareOPtions.miniProgram = {
+							id:'gh_33446d7f7a26',
+							path:'/pages/tabBar/component/component',
+							webUrl:'https://uniapp.dcloud.io',
+							type:0
+						};
+						break;
+					default:
+						break;
+				}
+				
+				if(shareOPtions.type === 0 && plus.os.name === 'iOS'){//如果是图文分享，且是ios平台，则压缩图片 
+					shareOPtions.imageUrl = await this.compress();
+				}
+				if(shareOPtions.type === 1 && shareOPtions.provider === 'qq'){//如果是分享文字到qq，则必须加上href和title
+					shareOPtions.href = 'https://uniapp.dcloud.io';
+					shareOPtions.title = '欢迎体验uniapp';
+				}
+				uni.share(shareOPtions);
+			},
+			compress() {//压缩图片 图文分享要求分享图片大小不能超过20Kb
+				console.log('开始压缩');
+				let img = this.image;
+				return new Promise((res) => {
+					var localPath = plus.io.convertAbsoluteFileSystem(img.replace('file://', ''));
+					console.log('after' + localPath);
+					// 压缩size
+					plus.io.resolveLocalFileSystemURL(localPath, (entry) => {
+						entry.file((file) => {// 可通过entry对象操作图片 
+							console.log('getFile:' + JSON.stringify(file));
+							if(file.size > 20480) {// 压缩后size 大于20Kb
+								plus.zip.compressImage({
+									src: img,
+									dst: img.replace('.jpg', '2222.jpg').replace('.JPG', '2222.JPG'),
+									width: '10%',
+									height: '10%',
+									quality: 1,
+									overwrite: true
+								}, (event) => {
+									console.log('success zip****' + event.size);
+									let newImg = img.replace('.jpg', '2222.jpg').replace('.JPG', '2222.JPG');
+									res(newImg);
+								}, function(error) {
+									uni.showModal({
+										content:'分享图片太大,需要请重新选择图片!',
+										showCancel:false
+									})
+								});
+							}
+						});
+					}, (e) => {
+						console.log('Resolve file URL failed: ' + e.message);
+						uni.showModal({
+							content:'分享图片太大,需要请重新选择图片!',
+							showCancel:false
+						})
+					});
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,39 +1,89 @@
 <template>
 	<view>
-		<common-list 
-			:item="info" 
-			isDetail 
-			@doShare="doShare" 
-			@doComment="doComment"
-			@follow="follow"
-			@onSupport="onSupport">
-			<view>{{info.content}}</view>
-			<view>
-				<image 
-					v-for="(item, index) in info.images" 
-					:key="index"
-					:src="item.url"
-					class="w-100"
-					mode="widthFix"
-					@click="preview(item)"></image>
+		<scroll-view scroll-y :style="'height:'+ screenH +'px;'">
+			<common-list
+				:item="info" 
+				isDetail 
+				@doShare="doShare" 
+				@doComment="doComment"
+				@follow="follow"
+				@onSupport="onSupport">
+				<view>{{info.content}}</view>
+				<view>
+					<image 
+						v-for="(item, index) in info.images" 
+						:key="index"
+						:src="item.url"
+						class="w-100"
+						mode="widthFix"
+						@click="preview(item)"></image>
+				</view>
+			</common-list>
+			<!-- 评论 -->
+			<view class="p-2 font font-weight-bold">热门评论 3</view>
+			<view class="px-2" style="height: 160rpx;">
+				<view class="uni-comment-list">
+					<view class="uni-comment-face">
+						<image src="/static/demo/demo6.jpg" style="width: 80rpx;height: 80rpx;"></image>
+					</view>
+					<view class="uni-comment-body">
+						<view class="uni-comment-top">15206774445</view>
+						<view class="uni-comment-content">嘿嘿</view>
+						<view class="uni-comment-date">
+							<view>两天前</view>
+						</view>
+					</view>
+				</view>
 			</view>
-		</common-list>
-		<divider></divider>
-		
-		
-		
+			<view class="px-2" style="height: 160rpx;">
+				<view class="uni-comment-list">
+					<view class="uni-comment-face">
+						<image src="/static/demo/demo6.jpg" style="width: 80rpx;height: 80rpx;"></image>
+					</view>
+					<view class="uni-comment-body">
+						<view class="uni-comment-top">15206774445</view>
+						<view class="uni-comment-content">嘿嘿</view>
+						<view class="uni-comment-date">
+							<view>两天前</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			<view class="px-2" style="height: 160rpx;">
+				<view class="uni-comment-list">
+					<view class="uni-comment-face">
+						<image src="/static/demo/demo6.jpg" style="width: 80rpx;height: 80rpx;"></image>
+					</view>
+					<view class="uni-comment-body">
+						<view class="uni-comment-top">15206774445</view>
+						<view class="uni-comment-content">嘿嘿</view>
+						<view class="uni-comment-date">
+							<view>两天前</view>
+						</view>
+					</view>
+				</view>
+			</view>
+		</scroll-view>
 		<!-- 底部输入栏 -->
 		<bottom-input @submit="submit"></bottom-input>
+		<!-- 分享弹出 -->
+		<more-share ref="popup" :providerList="providerList"></more-share>
 	</view>
 </template>
 
 <script>
 	import commonList from '@/components/common/common-list.vue'
 	import bottomInput from '@/components/common/bottom-input.vue'
+	import moreShare from '@/components/common/more-share.vue'
 	export default {
-		components: { commonList, bottomInput },
+		components: { 
+			commonList,
+			bottomInput,
+			moreShare
+		},
 		data() {
 			return {
+				screenH: '',
 				info: {
 					username: '王五', // 用户名
 					userpic: '/static/default.jpg', // 用户头像
@@ -57,13 +107,99 @@
 							url: '/static/demo/datapic/27.jpg'
 						}
 					]
-				}
+				},
+				providerList: []
 			}
+		},
+		onReady() {
+			uni.getProvider({
+				service: 'share',
+				success: (e) => {
+					let data = []
+					for (let i = 0; i < e.provider.length; i++) {
+						switch (e.provider[i]) {
+							case 'weixin':
+								data.push({
+									name: '微信好友',
+									id: 'weixin',
+									icon: 'iconfont icon-weixin',
+									bgColor: 'background-color: #2AD19B;',
+									sort:0
+								})
+								data.push({
+									name: '朋友圈',
+									id: 'weixin',
+									icon: 'uni-icon uni-icon-pengyouquan',
+									bgColor: 'background-color: #514D4C;',
+									type:'WXSenceTimeline',
+									sort:1
+								})
+								break;
+							case 'sinaweibo':
+								data.push({
+									name: '新浪微博',
+									id: 'sinaweibo',
+									icon: 'iconfont icon-xinlangweibo',
+									bgColor: 'background-color: #EE5E5E;',
+									sort:2
+								})
+								break;
+							case 'qq':
+								data.push({
+									name: '分享到QQ',
+									id: 'qq',
+									icon: 'iconfont icon-QQ',
+									bgColor: 'background-color: #5280CE;',
+									sort:3
+								})
+								break;
+							default:
+								break;
+						}
+					}
+					this.providerList = data.sort((x,y) => {
+						return x.sort - y.sort
+					});
+				},
+				fail: (e) => {
+					uni.showModal({
+						content:'获取分享通道失败',
+						showCancel:false
+					})
+				}
+			});
+		},
+		// 微信小程序
+		onShareAppMessage() {
+			return {
+				title: this.shareText ? this.shareText : "欢迎体验uni-app",
+				path: '/pages/tabBar/component/component',
+				imageUrl:this.image ? this.image : 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/app/share-logo@3.png'
+			}
+		},
+		// 当组件销毁之前
+		beforeDestroy() {
+			this.shareText='uni-app可以同时发布成原生App、小程序、H5，邀请你一起体验！',
+			this.href = 'https://uniapp.dcloud.io',
+			this.image='';
 		},
 		onLoad(value) {
 			if(value.detail) {
 				this.__init(JSON.parse(value.detail))
 			}
+			uni.getSystemInfo({
+				success: (res) => {
+					this.screenH = res.screenHeight - uni.upx2px(250)
+				}
+			})
+		},
+		// 监听顶部导航右侧按钮的点击
+		onNavigationBarButtonTap() {
+			this.$refs.popup.open()
+		},
+		// 监听返回 并对出底部弹出
+		onBackPress() {
+			this.$refs.popup.close()
 		},
 		methods: {
 			__init(data) {
@@ -128,8 +264,8 @@
 			},
 			submit(data) {
 				console.log(data);
-			}
-		},
+			},
+		},		
 		computed: {
 			imageList() {
 				return this.info.images.map(item => item.url)
